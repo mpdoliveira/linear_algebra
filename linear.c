0f1cd1a3e0b31d69c5a87cbf70473fe0ff5cb8c1
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-14 19:40:22 -0300

    feat: sum matrixes

--- linear.c
+++ linear.c
@@ -24,6 +24,22 @@ matrix *new_matrix(int lines, int columns)
     return m;
 }
 
+matrix *sum_matrix(matrix *m0, matrix *m1) {
+    if (m0->lines != m1->lines || m0->columns != m1->columns) {
+        retunr NULL;
+    }
+
+    matrix *sum = new_matrix(m0->lines, m0->columns);
+
+    for (int i = 0; i < sum->lines; i++) {
+        for (int j = 0; j < sum->columns; j++) {
+            sum->values[i][j] = m0->values[i][j] + m1->values[i][j];
+        }
+    }
+
+    return sum;
+}
+
 matrix *mult_matrix(matrix *m0, matrix *m1)
 {
     if (m0->columns != m1->lines)
@@ -36,7 +52,7 @@ matrix *mult_matrix(matrix *m0, matrix *m1)
     for (int i = 0; i < mult->lines; i++)
     {
         for (int j = 0; j < mult->columns; j++)
-        {
+        { 
             for (int k = 0; k < m0->columns; k++)
             {
                 mult->values[i][j] += m0->values[i][k] * m1->values[k][1];
