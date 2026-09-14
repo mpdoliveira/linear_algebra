0a4767560647c831b5cd6867c322f7cefbfde6bd
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-14 19:34:37 -0300

    feat: matrix multiplication

--- linear.c
+++ linear.c
@@ -23,3 +23,26 @@ matrix *new_matrix(int lines, int columns)
     }
     return m;
 }
+
+matrix *mult_matrix(matrix *m0, matrix *m1)
+{
+    if (m0->columns != m1->lines)
+    {
+        return NULL;
+    }
+
+    matrix *mult = new_matrix(m0->lines, m1->columns);
+
+    for (int i = 0; i < mult->lines; i++)
+    {
+        for (int j = 0; j < mult->columns; j++)
+        {
+            for (int k = 0; k < m0->columns; k++)
+            {
+                mult->values[i][j] += m0->values[i][k] * m1->values[k][1];
+            }
+        }
+    }
+
+    return mult;
+}
