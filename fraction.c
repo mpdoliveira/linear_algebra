1686c1f5e661c2cecb8a90944c91f4cb6c8c99d3
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-16 14:45:19 -0300

    feat: fraction multiplication function

--- fraction.c
+++ fraction.c
@@ -19,3 +19,7 @@ Fraction frac_add(Fraction frac1, Fraction frac2)
     return F((frac1.n * frac2.d) + (frac2.n * frac1.d), frac1.d * frac2.d);
 }
 
+Fraction frac_mul(Fraction frac1, Fraction frac2)
+{
+    return F(frac1.n * frac2.n, frac1.d * frac2.d);
+}
